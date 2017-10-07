cb77fb749902f52d1f83544c5d8aa543c93c074c
Devayan Sarkar <devayansarkar@gmail.com>
2017-10-07 23:44:57 +0200

    adding basic app ts

---
+++ src/App.ts
@@ -0,0 +1,21 @@
+import * as express from 'express'
+
+class App {
+    public express
+
+    constructor() {
+        this.express = express()
+        this.mountRoutes()
+    }
+
+    private mountRoutes(): void {
+        const router = express.Router()
+
+        router.get('/', (req, res) => {
+            res.json({ status: 'OK' })
+        })
+
+        this.express.use('/', router)
+
+    }
+}
